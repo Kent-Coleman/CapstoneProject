2220907a83b78bee3360d6afaae40dfcd5f36749
Kent Coleman <coleman.a.kent@gmail.com>
2023-09-10 19:04:56 -0400

    Fixed bug preventing rowHtml function being recognized

--- WorkoutTracker.js
+++ WorkoutTracker.js
@@ -33,24 +33,28 @@ export default class WorkoutTracker {
 
     static rowHtml() {
         return `
-            <table class="tracker">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Workout</th>
-                        <th>Duration</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody class="tracker__entries"></tbody>
-                <tbody>
-                    <tr class="tracker__row tracker__row--add">
-                        <td colspan="4">
-                            <button type="button" class="tracker__add">Add Entry &plus;</span>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <tr class="tracker__row">
+                <td>
+                    <input type="date" class="tracker__date">
+                </td>
+                <td>
+                    <select class="tracker__workout">
+                        <option value="walking">Walking</option>
+                        <option value="running">Running</option>
+                        <option value="cycling">Cycling</option>
+                        <option value="yoga">Yoga</option>
+                        <option value="swimming">Swimming</option>
+                        <option value="strength-exercise">Strength Exercise</option>
+                    </select>
+                </td>
+                <td>
+                    <input type="number" class="tracker__duration">
+                    <span class="tracker__text">minutes</span>
+                </td>
+                <td>
+                    <button type="button" class="tracker__button">&times;</button>
+                </td>
+            </tr>
         `;
     }
 
@@ -68,16 +72,29 @@ export default class WorkoutTracker {
             const template = document.createElement("template");
             let row = null;
 
-            template.innerHTML = WorkoutTracker.rowHTML().trim();
+            template.innerHTML = WorkoutTracker.rowHtml().trim();
             row = template.content.firstElementChild;
+
+            row.querySelector(".tracker__date").value = data.date;
+            row.querySelector(".tracker__workout").value = data.workout;
+            row.querySelector(".tracker__duration").value = data.duration;
+
+            // TODO: add events for input changing
+            // TODO: add event to delete row
 
             tableBody.appendChild(row);
         };
 
         tableBody.querySelectorAll(".tracker__row").forEach(row => {
-            row.delete();
+            row.remove();
         })
 
         this.entries.forEach(data => addRow(data));
+    }
+
+    addEntry(data) {
+        this.entries.push(data);
+        this.saveEntries();
+        this.updateView();
     }
 }
