70d20f6dfa02cf1e0a53918a6bae37eb70d29db1
Kent Coleman <coleman.a.kent@gmail.com>
2023-09-10 18:47:15 -0400

    Defined rowHTML method

--- WorkoutTracker.js
+++ WorkoutTracker.js
@@ -31,6 +31,29 @@ export default class WorkoutTracker {
         `;
     }
 
+    static rowHtml() {
+        return `
+            <table class="tracker">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Workout</th>
+                        <th>Duration</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody class="tracker__entries"></tbody>
+                <tbody>
+                    <tr class="tracker__row tracker__row--add">
+                        <td colspan="4">
+                            <button type="button" class="tracker__add">Add Entry &plus;</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        `;
+    }
+
     loadEntries() {
         this.entries = JSON.parse(localStorage.getItem("workout-tracker-entries") || "[]");
     }
