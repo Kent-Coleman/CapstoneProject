fbdd931cb5a340edc25960062fe09eb199ee9fe1
Kent Coleman <coleman.a.kent@gmail.com>
2023-09-10 18:25:08 -0400

    Moved HTML component to js

--- WorkoutTracker.js
+++ WorkoutTracker.js
@@ -1,3 +1,29 @@
 export default class WorkoutTracker {
+    constructor(root) {
+        this.root = root;
+        this.root.insertAdjacentHTML("afterbegin", WorkoutTracker.html());
+    }
 
+    static html() {
+        return `
+            <table class="tracker">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Workout</th>
+                        <th>Duration</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody class="tracker__entries"></tbody>
+                <tbody>
+                    <tr class="tracker__row tracker__row--add">
+                        <td colspan="4">
+                            <button type="button" class="tracker__add">Add Entry &plus;</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        `;
+    }
 }
